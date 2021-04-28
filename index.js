0299a500a1eac4f2b1df0bb41063f3077aad090f
Chrisi <81706150+Chrisi816@users.noreply.github.com>
2021-04-28 19:20:44 +0200

    Update index.js

--- index.js
+++ index.js
@@ -111,6 +111,21 @@ client.on('ready', async () => {
          message.channel.send(`Du hast ${zahl} Ehre`)
 
     })
+        client.on('message', async message => {
+        if (message.content === '!meme') {
+          let subreddits = [
+            "memes"
+          ];
+          let subreddit = subreddits[Math.floor(Math.random()*(subreddits.length))];
+          let img = await api(subreddit)
+          const Embed = new Discord.MessageEmbed()
+          .setTitle(`Ein Random Reddit Meme <3  `)
+          .setURL(`https://www.reddit.com/r/arabfunny`)
+          .setColor('RANDOM')
+          .setImage(img)
+          message.channel.send(Embed)
+        }
+      });
 
 
     // !member -> So viele Member sind aufm Server von vAzoniq!
