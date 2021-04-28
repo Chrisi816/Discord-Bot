fc485d29642b4e1268ab88b06cc7a1f34602cc63
Chrisi <81706150+Chrisi816@users.noreply.github.com>
2021-04-28 19:23:55 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const fs = require ('fs')
 const Discord = require ('discord.js')
 const client = new Discord.Client({partials: ['MESSAGE', 'CHANNEL', 'REACTION']})
 const util = require('minecraft-server-util');
+const api = require("imageapi.js")
 
 const config = require('./config.json')
 const PREFIX = '!'
