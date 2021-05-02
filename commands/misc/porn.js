964d64c97a23dd078d80879eeb7c86d47d4de76c
Chrisi <81706150+Chrisi816@users.noreply.github.com>
2021-05-02 20:56:26 +0200

    Update porn.js

--- commands/misc/porn.js
+++ commands/misc/porn.js
@@ -4,7 +4,7 @@ const nsfw = new NSFW();
 
 module.exports = {
     commands: ['porn'],
-    cooldown: 10,
+    cooldown: 4,
     description: 'Ein Pussy Bild wird gepostet!',
     callback: async (message, discord) => {
         const image = await nsfw.pgif();
