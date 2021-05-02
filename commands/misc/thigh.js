a33841cc572aeb48e5b0d0213e60b0a1e3d6cbe9
Chrisi <81706150+Chrisi816@users.noreply.github.com>
2021-05-02 20:57:02 +0200

    Update thigh.js

--- commands/misc/thigh.js
+++ commands/misc/thigh.js
@@ -5,7 +5,7 @@ const nsfw = new NSFW();
 module.exports = {
     commands: ['thigh'],
     description: 'thigh',
-    cooldown: 10,
+    cooldown: 4,
     callback: async (message, discord) => {
         const image = await nsfw.thigh();
         const embed = new Discord.MessageEmbed()
