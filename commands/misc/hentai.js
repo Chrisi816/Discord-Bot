280b0db020e397983eb91a0a308d91a96667b00e
Chrisi <81706150+Chrisi816@users.noreply.github.com>
2021-05-02 20:56:12 +0200

    Update hentai.js

--- commands/misc/hentai.js
+++ commands/misc/hentai.js
@@ -5,7 +5,7 @@ const nsfw = new NSFW();
 module.exports = {
     commands: ['hentai'],
     description: 'hentai',
-    cooldown: 10,
+    cooldown: 4,
     callback: async (message, discord) => {
         const image = await nsfw.hentai();
         const embed = new Discord.MessageEmbed()
