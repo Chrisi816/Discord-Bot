5074f85d4649c781fe0cd3dd1810edb80b65b93a
Chrisi <81706150+Chrisi816@users.noreply.github.com>
2021-05-19 22:06:05 +0200

    Delete schemas directory

--- schemas/member-count.js
+++
@@ -1,9 +0,0 @@
-const { Schema, model } = require('mongoose');
-
-module.exports = model('member-count', 
-    new Schema({
-        Guild: String,
-        Channel: String,
-        Member: String,
-    })
-)
