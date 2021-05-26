f593c2c33aa6bfc1ca8721a2f71acf7a410d0169
Chrisi <81706150+Chrisi816@users.noreply.github.com>
2021-05-26 21:16:10 +0200

    Add files via upload

--- mongo/level.js
+++ mongo/level.js
@@ -5,7 +5,7 @@ module.exports = (client) => {
     client.on('message', (message) => {
         const { guild, member } = message
         if(message.author.bot) return;
-        addXP(guild.id, member.id, Math.floor(Math.random() * 15) + 13, message)
+        addXP(guild.id, member.id, Math.floor(Math.random() * 12) + 10, message)
     })
 }
 
@@ -37,6 +37,79 @@ const addXP = async (guildId, userId, xpToAdd, message) => {
              xp -= needed
 
              message.reply(`Du bist jetzt Level ${level}!`)
+
+             if(level == 1) {
+                let role = message.guild.roles.cache.find(role => role.name == "Anfänger [1] ")
+                if (!role) await message.guild.roles.create({
+                    data: {
+                    name: "Level 1",
+                    color: "GREY",
+                   }
+                }).catch(err => console.log(err))
+                role = message.guild.roles.cache.find(role => role.name == "Anfänger [1] ")
+                if(message.member.roles.cache.has(role.id)) return
+                else await message.member.roles.add(role.id)
+            }
+            if(level == 5) {
+                let role = message.guild.roles.cache.find(role => role.name == "Erfahrender [5]")
+                if (!role) await message.guild.roles.create({
+                    data: {
+                    name: "Erfahrender [Level 5]",
+                    color: "GREY",
+                   }
+                }).catch(err => console.log(err))
+                role = message.guild.roles.cache.find(role => role.name == "Erfahrender [5]")
+                if(message.member.roles.cache.has(role.id)) return
+                else await message.member.roles.add(role.id)
+            }
+            if(level == 10) {
+                let role = message.guild.roles.cache.find(role => role.name == "Master[10]")
+                if (!role) await message.guild.roles.create({
+                    data: {
+                    name: "Master[10]",
+                    color: "GREY",
+                   }
+                }).catch(err => console.log(err))
+                role = message.guild.roles.cache.find(role => role.name == "Master[10]")
+                if(message.member.roles.cache.has(role.id)) return
+                else await message.member.roles.add(role.id)
+            }
+            if(level == 15) {
+                let role = message.guild.roles.cache.find(role => role.name == "Lord [15]")
+                if (!role) await message.guild.roles.create({
+                    data: {
+                    name: "Lord [15]",
+                    color: "GREY",
+                   }
+                }).catch(err => console.log(err))
+                role = message.guild.roles.cache.find(role => role.name == "Lord [15]")
+                if(message.member.roles.cache.has(role.id)) return
+                else await message.member.roles.add(role.id)
+            }
+            if(level == 25) {
+                let role = message.guild.roles.cache.find(role => role.name == "XP Glitcher [25]")
+                if (!role) await message.guild.roles.create({
+                    data: {
+                    name: "XP Glitcher [25]",
+                    color: "GREY",
+                   }
+                }).catch(err => console.log(err))
+                role = message.guild.roles.cache.find(role => role.name == "XP Glitcher [25]")
+                if(message.member.roles.cache.has(role.id)) return
+                else await message.member.roles.add(role.id)
+            }
+            if(level == 50) {
+                let role = message.guild.roles.cache.find(role => role.name == "King of Chatting [50]")
+                if (!role) await message.guild.roles.create({
+                    data: {
+                    name: "King of Chatting [50]",
+                    color: "GREY",
+                   }
+                }).catch(err => console.log(err))
+                role = message.guild.roles.cache.find(role => role.name == "King of Chatting [50]")
+                if(message.member.roles.cache.has(role.id)) return
+                else await message.member.roles.add(role.id)
+            }
 
              await profileSchema.updateOne({
                  guildId,
