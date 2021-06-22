f4082933885eb05fe1b37a2c8b0fa785f7fb9d90
Chrisi <81706150+Chrisi816@users.noreply.github.com>
2021-06-22 18:37:29 +0200

    Add files via upload

---
+++ commands/admin/antislowmode.js
@@ -0,0 +1,16 @@
+const discord = require('discord.js')
+
+module.exports = {
+    commands:"antislowmode",
+    callback: async(message, args) => {
+
+        message.channel.setRateLimitPerUser(0)
+
+        const embed = new discord.MessageEmbed()
+                 .setTitle('**Slowmode**')
+                 .addField(`Infos`, `Dieser Channel wurde wieder dem Slowmode erlöst.`)
+
+                 message.channel.send(embed)
+    },
+    permissions: 'ADMINISTRATOR'
+}
---
+++ commands/admin/slowmode.js
@@ -0,0 +1,23 @@
+const discord = require('discord.js')
+const { permissions } = require('./clear50')
+
+module.exports = {
+    commands:"slowmode",
+    callback: async(message, args) => {
+
+        const time = args[0]
+        const grund = args[1]
+        if(!time) return message.channel.send("Bitte gebe eine Zeit in Sekunden an.")
+        if(isNaN(time)) return message.channel.send("Bitte gebe eine ordnungsgemäße Sekunden Anzahl an!")
+        if(!grund) return message.channel.send("Bitte geben sie ein Grund an.")
+
+        message.channel.setRateLimitPerUser(time, 'No Reason')
+
+        const embed = new discord.MessageEmbed()
+                 .setTitle('**Slowmode**')
+                 .addField(`Infos`, `Dieser Channel wurde in den Slowmode gesetzt da ${grund}, ihr könnt jetzt alle ${time}s etwas  schreiben.`)
+
+                 message.channel.send(embed)
+    },
+    permissions: 'ADMINISTRATOR'
+}
