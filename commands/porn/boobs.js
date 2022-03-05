ae47ffb30673e5c13e29b92475c5ae9c41cc9f5e
Chrisi <81706150+Chrisi816@users.noreply.github.com>
2022-03-05 13:01:27 +0100

    Update boobs.js

--- commands/porn/boobs.js
+++ commands/porn/boobs.js
@@ -4,7 +4,7 @@ const nsfw = new NSFW();
 
 module.exports = {
     commands: ['Boobs'],
-    cooldown: 4,
+    cooldown: 0,
     description: 'Ein Boobs bild wird gepostet!',
     callback: async (message, discord) => {
         const image = await nsfw.boobs();
